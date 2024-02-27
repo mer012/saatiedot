ba27ac474528e52f60f84ea88d5b0c3c3f33d3b1
Merna Rofail <mernarofail@Merna-MacBook-Air.local>
2024-02-27 14:52:00 +0200

    valmis

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 const url = 'https://api.openweathermap.org/data/2.5/weather?'
 const icon_url = 'http://openweathermap.org/img/wn/'
-const api_key = 
+const api_key = 'a898aa82e3eda738618cb6bb18d9c326'
 
 const temp_span = document.querySelector('#temp')
 const speed_span = document.querySelector('#speed')
@@ -9,15 +9,12 @@ const description_span = document.querySelector('#description')
 const icon_img = document.querySelector('img')
 
 const getLocation = () => {
-    console.log('getLocation')
     if (navigator.geolocation) {
          navigator.geolocation.getCurrentPosition(position => {
-            console.log(position)
-            document.querySelector('#lat').innerHTML = position.coords.latitude.toFixed(3) + ','
+            document.querySelector('#lat').innerHTML = position.coords.latitude.toFixed(3) + ', '
             document.querySelector('#lng').innerHTML = position.coords.longitude.toFixed(3)
             getWeather(position.coords.latitude,position.coords.longitude)
         }),(error => {
-            console.log(error)
             alert(error)
         }) 
     } else {
@@ -35,9 +32,9 @@ const getWeather = (lat,lng) => {
     .then(response => {
         const json = response.data
         temp_span.innerHTML = json.main.temp + '&#8451;'
-        speed_span.innerHTML = json.speed + 'm/s'
+        speed_span.innerHTML = json.wind.speed + 'm/s'
         direction_span.innerHTML = json.wind.deg + '&#176;'
-        description_span.innerHTML = json.weather[0].description_span
+        description_span.innerHTML = json.weather[0].description
         const image = icon_url + json.weather[0].icon + '@2x.png'
         icon_img.src = image
     }).catch(error => {
