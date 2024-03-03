458cec0a7075b929de26c7b80d4c0f4fda24efdd
mer012 <156085633+mer012@users.noreply.github.com>
2024-03-03 19:55:11 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 const url = 'https://api.openweathermap.org/data/2.5/weather?'
 const icon_url = 'http://openweathermap.org/img/wn/'
-const api_key = 'a898aa82e3eda738618cb6bb18d9c326'
+const api_key = ''
 
 const temp_span = document.querySelector('#temp')
 const speed_span = document.querySelector('#speed')
